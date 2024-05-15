57052de645b190526a765b1bdc1e2e0f57dc3e68
Harris <harris.git21@gmail.com>
2024-05-15 21:33:21 +0700

    fix: change to midwife category

--- src/data/CoursesSection.jsx
+++ src/data/CoursesSection.jsx
@@ -1,6 +1,6 @@
 export const courseSection = {
     content : `<p className="deskripsi">You Will Need This</p>
-    <h2>Online Courses</h2>
+    <h2>Online Consultation</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt voluptatibus possimus blanditiis reiciendis. Qui, facilis! Delectus exercitationem dolores sapiente?</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt voluptatibus possimus blanditiis reiciendis. Qui, facilis! Delectus exercitationem dolores sapiente?</p>
     <p><a href="" className="tbl-biru">Pelajari Lebih Lanjut</a></p>`,
--- src/data/TutorsSection.jsx
+++ src/data/TutorsSection.jsx
@@ -1,24 +1,24 @@
 export const tutorsSection = {
-    content : `<p className="deskripsi">Our Top Tutors</p>
-    <h2>Tutors</h2>
+    content : `<p className="deskripsi">Our Top Midwife</p>
+    <h2>Midwife</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, optio!</p>`
 }
 
 export const tutorsList = [
     {
-        image:'https://dfu1k3y1rami2.cloudfront.net/wp-content/uploads/2014/07/26195109/2020_cb.jpg',
-        name:'Jason Lee Scott'
+        image:'https://img.freepik.com/free-photo/covid19-healthcare-workers-pandemic-concept-confident-smiling-asian-nurse-scrubs-standing-selfassured-cross-arms-chest-professional-doctor-ready-surgery-standing-white-background_1258-107804.jpg?t=st=1715782800~exp=1715786400~hmac=abf9ae70212af6441fb329dc1816b928ffbb5d90d99d2fda448919f3963ad842&w=1380',
+        name:'Monalisa'
     },
     {
-        image:'https://images.ctfassets.net/1wryd5vd9xez/4DxzhQY7WFsbtTkoYntq23/a4a04701649e92a929010a6a860b66bf/https___cdn-images-1.medium.com_max_2000_1_Y6l_FDhxOI1AhjL56dHh8g.jpeg',
-        name:'John Doe'
+        image:'https://img.freepik.com/free-photo/smiling-asian-doctor-female-nurse-holding-clipboard-pen-wearing-uniform-with-gloves-writing-pati_1258-83340.jpg?t=st=1715782836~exp=1715786436~hmac=f508ce9f2e7d166186c4835aad36a4b7634f3eab0ed5c49ea5b75b7b76c4c62a&w=1380',
+        name:'Aliya'
     },
     {
-        image:'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto/fc/3021752-inline-i-1-why-square-designed-its-new-offices-to-work-like-a-city.jpg',
-        name:'Michael Dell'
+        image:'https://img.freepik.com/free-photo/pensive-young-asian-female-doctor-holding-documents_1262-18267.jpg?t=st=1715783482~exp=1715787082~hmac=a52c02ccb9fcaa01e46406f4a2794fa585e686ae89d27c471405ae0462b91dfe&w=1380',
+        name:'Jenifer'
     },
     {
-        image:'https://blogs-images.forbes.com/jackkelly/files/2019/06/Jack-Kelly_avatar_1559658819-400x400.jpg',
-        name:'Bruce Wills'
+        image:'https://img.freepik.com/free-photo/smiling-cute-asian-female-doctor-nurse-medical-respirator-rubber-sterile-gloves-looking-pleas_1258-83289.jpg?t=st=1715783011~exp=1715786611~hmac=9e77072018fbdcaa9c061dfb4c327cf583b803da193d2c5d355c9d81851bc1fb&w=1380',
+        name:'Ayu'
     },
 ]
